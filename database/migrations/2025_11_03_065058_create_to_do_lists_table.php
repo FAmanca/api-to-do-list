cc3d647a2179d245e86ef47c8ab5bb23a584f963
FAmanca <fadlyamanca@gmail.com>
2025-11-03 14:28:51 +0700

    store rev

--- database/migrations/2025_11_03_065058_create_to_do_lists_table.php
+++ database/migrations/2025_11_03_065058_create_to_do_lists_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->enum('priority', ['low','medium', 'high'])->default('low');
             $table->date('due_date')->nullable();
-            $table->boolean('is_completed')->nullable()->default(0);
+            $table->boolean('is_completed')->default(0);
             $table->timestamps();
         });
     }
